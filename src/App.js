42a7963f99e671a985343c56b8d8100991770618
ranimounikakotakadi <ranimounikakotakadi@gmail.com>
2019-06-10 16:03:15 +0000

    some commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          test react app
+          test react my-app
         </a>
       </header>
     </div>
